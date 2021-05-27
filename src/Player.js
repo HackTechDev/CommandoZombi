e41acc2898e76d6970996c2eac5ded01f4394904
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2021-05-27 21:22:57 +0200

    [Add] RPG

---
+++ src/Player.js
@@ -0,0 +1,143 @@
+/**
+ *
+ *
+ */
+
+export default class Player {
+    constructor(scene, x, y) {
+      this.scene = scene;
+        
+
+      const anims = scene.anims;
+      anims.create({
+        key: "misa-left-walk",
+        frames: anims.generateFrameNames("atlas", {
+          prefix: "misa-left-walk.",
+          start: 0,
+          end: 3,
+          zeroPad: 3
+        }),
+        frameRate: 10,
+        repeat: -1
+      });
+      anims.create({
+        key: "misa-right-walk",
+        frames: anims.generateFrameNames("atlas", {
+          prefix: "misa-right-walk.",
+          start: 0,
+          end: 3,
+          zeroPad: 3
+        }),
+        frameRate: 10,
+        repeat: -1
+      });
+      anims.create({
+        key: "misa-front-walk",
+        frames: anims.generateFrameNames("atlas", {
+          prefix: "misa-front-walk.",
+          start: 0,
+          end: 3,
+          zeroPad: 3
+        }),
+        frameRate: 10,
+        repeat: -1
+      });
+      anims.create({
+        key: "misa-back-walk",
+        frames: anims.generateFrameNames("atlas", {
+          prefix: "misa-back-walk.",
+          start: 0,
+          end: 3,
+          zeroPad: 3
+        }),
+        frameRate: 10,
+        repeat: -1
+      });
+
+     this.sprite = scene.physics.add
+        .sprite(x, y, "atlas", "misa-front")
+        .setSize(30, 40)
+        .setOffset(10, 24);
+
+    this.keyD = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
+    this.keys = scene.input.keyboard.createCursorKeys();
+
+
+    this.keyDOnce = true;
+
+    }
+
+
+    freeze() {
+        this.sprite.body.moves = false;
+    }
+
+
+    update(time, delta) {
+        const keys = this.keys;
+        const keyD = this.keyD;
+
+        const sprite = this.sprite;
+        const speed = 175;
+        const prevVelocity = sprite.body.velocity.clone();
+
+
+        console.log(sprite.x + " " + sprite.y);
+        
+        if(keyD.isDown) {
+            
+            if (!this.keyDOnce) {
+                console.log('D key pressed')
+                this.keyDOnce = true;
+            }
+        }
+
+        if(keyD.isUp) {
+          this.keyDOnce = false;
+        }
+
+        sprite.body.setVelocity(0);
+
+        if (keys.left.isDown) {
+            sprite.body.setVelocityX(-speed);
+        } else if (keys.right.isDown) {
+            sprite.body.setVelocityX(speed);
+        }
+
+        if (keys.up.isDown) {
+            sprite.body.setVelocityY(-speed);
+        } else if (keys.down.isDown) {
+            sprite.body.setVelocityY(speed);
+        }
+
+        sprite.body.velocity.normalize().scale(speed);
+
+        if (keys.left.isDown) {
+            sprite.anims.play("misa-left-walk", true);
+        } else if (keys.right.isDown) {
+            sprite.anims.play("misa-right-walk", true);
+        } else if (keys.up.isDown) {
+            sprite.anims.play("misa-back-walk", true);
+        } else if (keys.down.isDown) {
+            sprite.anims.play("misa-front-walk", true);
+        } else {
+            sprite.anims.stop();
+
+            if (prevVelocity.x < 0) {
+                sprite.setTexture("atlas", "misa-left");
+            } else if (prevVelocity.x > 0) { 
+                sprite.setTexture("atlas", "misa-right");
+            } else if (prevVelocity.y < 0) { 
+                sprite.setTexture("atlas", "misa-back");
+            } else if (prevVelocity.y > 0) {
+                sprite.setTexture("atlas", "misa-front");
+            }
+        }
+    }
+
+    destroy() {
+        this.sprite.destroy();
+    }
+}
+
--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,3 +1,4 @@
+import Player from "../Player.js";
 import 'phaser';
 
 export default class GameScene extends Phaser.Scene {
@@ -5,12 +6,68 @@ export default class GameScene extends Phaser.Scene {
     super('Game');
   }
 
-  preload () {
-    // load images
-    this.load.image('logo', 'assets/logo.png');
-  }
+    preload() {
+        this.load.image("tiles", "../assets/tilesets/tuxmon-sample-32px-extruded.png");
+        this.load.tilemapTiledJSON("map", "../assets/tilemaps/tuxemon-town.json");
 
-  create () {
-    this.add.image(400, 300, 'logo');
-  }
-};
+         this.load.atlas("atlas", "./assets/atlas/atlas.png", "./assets/atlas/atlas.json");
+
+    }
+
+    create() {
+        const map = this.make.tilemap({ key: "map" });
+
+        const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles");
+
+        const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
+        const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
+        const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
+
+        worldLayer.setCollisionByProperty({ collides: true });
+
+        aboveLayer.setDepth(10);
+
+        const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
+
+        console.log(spawnPoint.x + " " + spawnPoint.y);
+
+        this.player = new Player(this, spawnPoint.x, spawnPoint.y);
+
+        this.physics.add.collider(this.player.sprite, worldLayer);
+
+        const camera = this.cameras.main;
+        camera.startFollow(this.player.sprite);
+        camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+        this.add
+            .text(16, 16, 'Move: Arrow keys', {
+                font: "18px monospace",
+                fill: "#000000",
+                padding: { x: 20, y: 10 },
+                backgroundColor: "#ffffff"
+            })
+        .setScrollFactor(0)
+        .setDepth(30);
+
+          this.input.keyboard.once("keydown_D", event => {
+            this.physics.world.createDebugGraphic();
+
+            const graphics = this.add
+              .graphics()
+              .setAlpha(0.75)
+              .setDepth(20);
+            worldLayer.renderDebug(graphics, {
+              tileColor: null, 
+              collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), 
+              faceColor: new Phaser.Display.Color(40, 39, 37, 255) 
+            });
+          });
+
+
+    }
+
+
+    update(time, delta) {
+        this.player.update();
+    }
+}
