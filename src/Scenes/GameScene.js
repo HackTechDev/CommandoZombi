d53851a4b603cef9c267ea0afa6bcf68983d39dd
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2021-05-28 00:17:47 +0200

    [Update] Asset path

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -7,10 +7,10 @@ export default class GameScene extends Phaser.Scene {
   }
 
     preload() {
-        this.load.image("tiles", "../assets/tilesets/tuxmon-sample-32px-extruded.png");
-        this.load.tilemapTiledJSON("map", "../assets/tilemaps/tuxemon-town.json");
+        this.load.image("tiles", "assets/tilesets/tuxmon-sample-32px-extruded.png");
+        this.load.tilemapTiledJSON("map", "assets/tilemaps/tuxemon-town.json");
 
-         this.load.atlas("atlas", "./assets/atlas/atlas.png", "./assets/atlas/atlas.json");
+         this.load.atlas("atlas", "assets/atlas/atlas.png", "assets/atlas/atlas.json");
 
     }
 
