be1b176643b55e547be723cb87cf908cf64f5b4d
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2021-06-09 23:24:08 +0200

    [Remove] Script

--- syncGame2Prod.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rsync -avz --exclude='.git/' --exclude='node_modules/' --exclude='script/' --exclude='src/' --delete phaser3_commandozombi/ nyrjjbc@ssh.cluster030.hosting.ovh.net:/homez.1509/nyrjjbc/commandozombi/
